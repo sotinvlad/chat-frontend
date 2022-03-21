03786658df985b457ec7a702d7fa172b48a4be6f
Vlad <sotinvlad@yandex.ru>
2022-03-21 13:39:27 +0300

    Загрузка файла в ChatInput

--- src/components/ChatInput/ChatInput.jsx
+++ src/components/ChatInput/ChatInput.jsx
@@ -1,52 +1,93 @@
 import React, { useState } from 'react';
-import Upload from 'rc-upload';
 import { AudioOutlined, SmileOutlined, PaperClipOutlined, ArrowRightOutlined } from '@ant-design/icons';
-import { Input, Button, Popover } from 'antd';
+import { Input, Button, Popover, Upload, message } from 'antd';
 import { connect } from 'react-redux';
 import Picker from 'emoji-picker-react';
 
 import './ChatInput.scss';
 import messagesActions from './../../redux/actions/messages';
 import messagesAPI from '../../utils/api/messagesAPI';
+import axios from '../../core/axios';
 
 
 const ChatInput = ({ userData, currentDialogId }) => {
 
     const [input, setInput] = useState('');
     const [showEmoji, setShowEmoji] = useState(false);
+    const [fileList, setFileList] = useState([]);
+    const [uploading, setUploading] = useState(false);
 
     const onEmojiClick = (event, emojiObject) => {
         setInput(input => input + emojiObject.emoji);
     };
 
     const onKeyUp = (e, input) => {
-        if (e.keyCode == 13 && input !== '')
+        if (e.keyCode == 13)
             sendMessage(input, currentDialogId, userData._id);
     }
 
     const sendMessage = (text, dialogId, userId) => {
-        messagesAPI.send({ text, dialogId, userId })
-            .then(data => console.log(data))
-            .catch(err => console.err(err));
-        setInput('');
+        if (text !== '' || fileList.length > 0){
+            messagesAPI.send({ text, dialogId, userId })
+            .then(data => {
+                console.log(data.data)
+                if (fileList.length > 0) {
+                    handleUpload(data.data._id);
+                }
+            })
+            .catch(err => console.log(err));
+            setInput('');
+        }
     }
 
-    const content = (
+    const contentForEmojiPopover = (
             <div className="chat-input__emoji">
                 <Picker onEmojiClick={onEmojiClick} />
             </div>
     )
 
+    const handleUpload = (messageId) => {
+        const formData = new FormData();
+        fileList.forEach(file => {
+            formData.append('files', file);
+        });
+        setUploading(true);
+        axios.post('http://localhost:5000/file/' + messageId, formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            }
+        }).then(res => {
+            setFileList([]);
+        }).catch(error => {
+            message.error(error);
+        }).finally(() => {
+            setUploading(false);
+        })
+    }
+
+    const uploadProps = {
+        onRemove: file => {
+            setFileList(fileList => {
+              const index = fileList.indexOf(file);
+              const newFileList = fileList.slice();
+              newFileList.splice(index, 1);
+              return newFileList;
+            });
+        },
+        beforeUpload: file => {
+            setFileList(fileList => [...fileList, file]);
+            return false;
+        },
+        fileList,
+    }
+
     return (
         <div className="chat-input">
-
-            <Popover content={content}>
-            <div className="chat-input__smile-btn" onClick={() => setShowEmoji(!showEmoji)}>
-                <SmileOutlined />
-            </div>
-
+            <Popover content={contentForEmojiPopover}>
+                <div className="chat-input__smile-btn" onClick={() => setShowEmoji(!showEmoji)}>
+                    <SmileOutlined />
+                </div>
             </Popover>
-
             <Input
                 onChange={e => setInput(e.target.value)}
                 onKeyUp={e => onKeyUp(e, input)}
@@ -55,21 +96,17 @@ const ChatInput = ({ userData, currentDialogId }) => {
                 value={input}
             />
             <div className="chat-input__buttons">
-                <Upload
-                    action={'/'}
-                    multiple={true}
-                    onStart={(file) => console.log('onStart', file, file.name)}
-                    onSuccess={(ret) => console.log('onSuccess', ret)}
-                    onError={(err) => console.log('onError', err)}
-                >
+                <Upload {...uploadProps} multiple={true}>
                     <PaperClipOutlined />
-                </Upload>
-                {input === '' ? <AudioOutlined /> :
+                </Upload>  
+                {input === '' && fileList.length === 0 ? <AudioOutlined /> :
                     <Button
                         type="primary"
                         shape="circle"
                         icon={<ArrowRightOutlined />}
                         size={'20px'}
+                        disabled={uploading}
+                        loading={uploading}
                         onClick={() => sendMessage(input, currentDialogId, userData._id)}
                     />}
             </div>
